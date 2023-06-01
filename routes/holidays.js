105ac5ed497ade86692d26cabfd73c42f643bd2a
Dragos-Calota <dragosalexandrucalota@gmail.com>
2023-06-01 13:06:35 +0300

    holidays

--- routes/holidays.js
+++ routes/holidays.js
@@ -9,7 +9,7 @@ const client = new MongoClient(process.env.DATABASE_URL);
 router.get("/", async (req, res) => {
   try {
     const db = client.db();
-    const collection = db.collection(process.env.events);
+    const collection = db.collection(process.env.holidays);
 
     const result = await collection.find().toArray();
 
@@ -22,11 +22,12 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const db = client.db();
-    const collection = db.collection(process.env.events);
+    const collection = db.collection(process.env.holidays);
 
     const result = await collection.findOne({
       _id: new ObjectId(req.params.id),
     });
+
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -34,40 +35,18 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const event = {
-    allDay: false,
-    title: `${req.body.subject}(${
-      req.body.type === "course"
-        ? "curs"
-        : req.body.type === "seminar"
-        ? "seminar"
-        : req.body.type === "lab"
-        ? "laborator"
-        : req.body.type === "project"
-        ? "proiect"
-        : null
-    })`,
-    rrule: {
-      dtstart: req.body.start,
-      freq: "weekly",
-      count: req.body.interval === 1 ? 14 : 7,
-      interval: req.body.interval,
-    },
-    duration: `0${req.body.duration}:00`,
-    extendedProps: {
-      teacher: req.body.teacher,
-      students: req.body.students,
-      classroom: req.body.classroom,
-      year: req.body.year,
-      series: req.body.series,
-    },
+  const holiday = {
+    start: req.body.start,
+    end: req.body.end,
+    display: "background",
+    color: "red",
   };
 
   try {
     const db = client.db();
-    const collection = db.collection(process.env.events);
+    const collection = db.collection(process.env.holidays);
 
-    const result = await collection.insertOne(event);
+    const result = await collection.insertOne(holiday);
     res.status(201).json(result);
   } catch (err) {
     res.status(400).json({ error: err.message });
@@ -77,7 +56,7 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const db = client.db();
-    const collection = db.collection(process.env.events);
+    const collection = db.collection(process.env.holidays);
 
     const result = await collection.deleteOne({
       _id: new ObjectId(req.params.id),
@@ -92,19 +71,22 @@ router.delete("/:id", async (req, res) => {
 router.patch("/:id", async (req, res) => {
   try {
     const db = client.db();
-    const collection = db.collection(process.env.events);
+    const collection = db.collection(process.env.holidays);
 
     const result = await collection.updateOne(
       { _id: new ObjectId(req.params.id) },
       {
         $set: {
-          "rrule.dtstart": req.body.start,
+          start: req.body.start,
+          end: req.body.end,
         },
       }
     );
 
     res.status(200).json(result);
-  } catch (err) {}
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
